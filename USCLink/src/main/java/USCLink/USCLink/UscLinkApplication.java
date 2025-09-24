6eaf4a632ee1376d235d0e3a17b558fa9a747b06
Daniel Vilariño <vilarinodaniel16@gmail.com>
2025-09-24 10:08:40 +0200

    Spring Boot

---
+++ USCLink/src/main/java/USCLink/USCLink/UscLinkApplication.java
@@ -0,0 +1,13 @@
+package USCLink.USCLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UscLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UscLinkApplication.class, args);
+	}
+
+}
