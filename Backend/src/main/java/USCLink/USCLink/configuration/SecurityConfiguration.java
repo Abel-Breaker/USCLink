752230e78a93b138ad0ef25850e63d66641539dc
vilarino16 <vilarinodaniel16@gmail.com>
2025-11-19 13:18:52 +0100

    Swagger works

--- Backend/src/main/java/USCLink/USCLink/configuration/SecurityConfiguration.java
+++ Backend/src/main/java/USCLink/USCLink/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import USCLink.USCLink.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,6 +34,7 @@ public class SecurityConfiguration {
         return http.authorizeHttpRequests(authorize ->
                         authorize.requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/media/**", "/public/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/v3/api-docs", "/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
 
                 )
